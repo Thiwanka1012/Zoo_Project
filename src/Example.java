bb008ccd1ec000e5a1d8888017fb4d8f1eef4df7
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-12-31 17:59:22 +0530

    Create A class

--- src/Example.java
+++ src/Example.java
@@ -1,2 +1,41 @@
+//Generics
+
+
+class Lion{
+    String details="Lion Details";
+}
+
+class Elephant{
+    String details="Elephant Details";
+}
+
+class Fish{
+    String details="Fish Details";
+}
+
+class PrintInformation{
+    String details;
+
+    PrintInformation(Lion lion){
+        details=lion.details;
+    }
+
+    public void printData(){
+        System.out.println(details);
+    }
+}
+
+
+
+
+
+
+
 public class Example {
+    public static void main(String[] args) {
+
+        Lion lion=new Lion();
+        PrintInformation information=new PrintInformation(lion);
+        information.printData();
+    }
 }
